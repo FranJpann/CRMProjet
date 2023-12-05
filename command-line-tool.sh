a7db549a5f1fa894a8bd3ba70f03290b652f8dd9
FranJpann <loann.cady@gmail.com>
2023-12-05 23:28:31 +0100

    add command-line-tool

---
+++ command-line-tool.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+findLeads() {
+  api_url="http://localhost:8080/findLeads?lowAnnualRevenue=${lowAnnualRevenue}&highAnnualRevenue=${highAnnualRevenue}&state=${state}"
+
+  result=$(curl -s "$api_url")
+  echo "$result"
+}
+
+findLeadsByDate() {
+  api_url="http://localhost:8080/findLeadsByDate?startDate=${startDate}&endDate=${endDate}"
+
+  result=$(curl -s $api_url)
+  echo "$result"
+}
+
+getToken() {
+  read -p "Entrez votre login salesforce : " login
+  read -s -p "Entrez votre mot de passe : " password
+  echo
+
+  result=$(curl --request POST 'https://login.salesforce.com/services/oauth2/token' --header 'Content-Type: application/x-www-form-urlencoded' --data-urlencode 'grant_type=password' --data-urlencode 'client_id=3MVG9XLfsJ3MQLvysrUmz9Ib74AMdcwELVowIuBXxwcRRR2vcWh5wYv7.teloCTpnKQn6fQX6wr1qFgekjoSZ' --data-urlencode 'client_secret=2F60A3BA3D0AB8A632C6E11007EC2BF9775CF599048B7DC0D70D7EE791344869'  --data-urlencode "username=${login}" --data-urlencode "password=${password}")
+  echo "$result"
+}
+
+if [ "$1" == "findLeads" ]; then
+  lowAnnualRevenue=$2
+  highAnnualRevenue=$3
+  state=$4
+  findLeads
+elif [ "$1" == "findLeadsByDate" ]; then
+  startDate=$2
+  endDate=$3
+  findLeadsByDate
+elif [ "$1" == "getToken" ]; then
+  getToken
+else
+  echo "Fonction inconnue : $1. Veuillez spécifier une commande correcte (findLeads, findLeadsByDate, getToken)."
+  exit 1
+fi
--- creditential.sh
+++
@@ -1,2 +0,0 @@
-curl -G "https://univangers-dev-ed.develop.my.salesforce.com/services/data/v45.0/query/" --header 'Content-Type: application/x-www-form-urlencoded' --header 'Authorization: Bearer 00D07000000YPGQ!AR4AQMJIDt7XN2KWJQhvroDscGQTOcDzbAaetatv78x1JxOytzfA6xNo6nfo87OSME9npBsvUWtdEbqnUXw3JUYqNzxxqQfn'  --header "Accept: application/json" -d "q=SELECT+AnnualRevenue+FROM+Lead"
-
