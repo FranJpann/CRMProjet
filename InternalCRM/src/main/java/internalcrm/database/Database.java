3642bff21c6569d3c9eaa5080a28128037016995
FranJpann <loann.cady@gmail.com>
2023-12-05 11:52:31 +0100

    Modif Date dd-MM-yyyy -> yyyy-MM-dd

--- InternalCRM/src/main/java/internalcrm/database/Database.java
+++ InternalCRM/src/main/java/internalcrm/database/Database.java
@@ -15,8 +15,8 @@ public class Database {
     public Database() {
         models = new ArrayList<>();
 
-        models.add(new Model("Loann", "Cady", 0.0, "0749471448", "15 rue St Gilles", "49124", "Le Plessis-Grammoire", "France", "30-11-2023", "UnivAngers", "France"));
-        models.add(new Model("Baptiste", "Chaussee", 1300.0 , "0749471448", "19 rue des coquelicots", "49570", "Montjean", "France", "30-11-2023", "UnivAngers", "France"));
+        models.add(new Model("Loann", "Cady", 0.0, "0749471448", "15 rue St Gilles", "49124", "Le Plessis-Grammoire", "France", "2023-11-30", "UnivAngers", "France"));
+        models.add(new Model("Baptiste", "Chaussee", 1300.0 , "0749471448", "19 rue des coquelicots", "49570", "Montjean", "France", "2023-11-30", "UnivAngers", "France"));
     }
 
     public void addModel(Model model) {
@@ -41,7 +41,7 @@ public class Database {
     public List<Model> getModelsByDate(String startDate, String endDate) throws ParseException {
         List<Model> modelResults = new ArrayList<>();
 
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         Date startD = simpleDateFormat.parse(startDate);
         Date endD = simpleDateFormat.parse(endDate);
 
