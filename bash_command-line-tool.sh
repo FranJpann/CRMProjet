c8b68c73897a4111231699ee3bd3c703db0980f2
FranJpann <loann.cady49@gmail.com>
2024-01-15 10:00:07 +0100

    changes commandlinetool

--- bash_command-line-tool.sh
+++ bash_command-line-tool.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
 findLeads() {
-  api_url="http://localhost:8080/findLeads?lowAnnualRevenue=${lowAnnualRevenue}&highAnnualRevenue=${highAnnualRevenue}&state=${state}"
-
-  result=$(curl -s "$api_url")
-  echo "$result"
+  ./gradlew :CommandLineTool:run --args="findLeads ${lowAnnualRevenue} ${highAnnualRevenue} ${state}"
 }
 
 findLeadsByDate() {
-  api_url="http://localhost:8080/findLeadsByDate?startDate=${startDate}&endDate=${endDate}"
-
-  result=$(curl -s $api_url)
-  echo "$result"
+  ./gradlew :CommandLineTool:run --args="findLeadsByDate ${startDate} ${endDate}"
 }
 
-getToken() {
-  read -p "Entrez votre login salesforce : " login
-  read -s -p "Entrez votre mot de passe : " password
-  echo
-
-  result=$(curl --request POST 'https://login.salesforce.com/services/oauth2/token' --header 'Content-Type: application/x-www-form-urlencoded' --data-urlencode 'grant_type=password' --data-urlencode 'client_id=3MVG9XLfsJ3MQLvysrUmz9Ib74AMdcwELVowIuBXxwcRRR2vcWh5wYv7.teloCTpnKQn6fQX6wr1qFgekjoSZ' --data-urlencode 'client_secret=2F60A3BA3D0AB8A632C6E11007EC2BF9775CF599048B7DC0D70D7EE791344869'  --data-urlencode "username=${login}" --data-urlencode "password=${password}")
-  echo "$result"
+appRun() {
+  ./gradlew --parallel :VirtualCRM:appRun :InternalCRM:run
 }
 
 if [ "$1" == "findLeads" ]; then
@@ -32,15 +21,10 @@ elif [ "$1" == "findLeadsByDate" ]; then
   startDate=$2
   endDate=$3
   findLeadsByDate
-elif [ "$1" == "getToken" ]; then
-  getToken
-elif [ "$1" == "runDocker" ]; then
-  sudo docker-compose build
-  sudo docker-compose up
 elif [ "$1" == "appRun" ]; then
-  ./gradlew --parallel :VirtualCRM:appRun :InternalCRM:run
+  appRun
 elif [ "$1" == "-h" ] || [ "$1" == "-help" ]; then
-    printf "Fonctions disponibles : \n findLeads (lowAnnualRevenue) (highAnnualRevenue) (state) \n findLeadsByDate (YYYY-MM-dd) (YYYY-MM-dd) \n getToken \n runDocker (Il faut lancer le daemon Docker) \n appRun"
+    printf "Fonctions disponibles : \n findLeads (lowAnnualRevenue) (highAnnualRevenue) (state) \n findLeadsByDate (YYYY-MM-dd) (YYYY-MM-dd) \n appRun"
 else
   echo "Fonction inconnue : $1. Veuillez utiliser -h ou -help pour voir les commandes disponibles."
   exit 1
